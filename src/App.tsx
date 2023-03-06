1b9bac1f0add1072af6494d0ddac771fd15a9346
Lootmann <ryota.tsugawa.sites.tabs@gmail.com>
2023-03-07 07:50:31 +0900

    ReChart - Re-reformat structured and rename

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import axios from "axios";
 import Header from "./header";
-import Rechart from "./Rechart";
+
+// Rechart
+import CustomBarChart from "./Rechart/CustomBarChart";
+import CustomPieChart from "./Rechart/CustomPieChart";
 
 // React Chart.JS
 import BarChart from "./ReactChart/BarChart";
@@ -28,18 +31,17 @@ function App() {
 
       <div className="h-[calc(100vh-4rem)] flex gap-4 bg-slate-600 p-4">
         <div className="flex flex-col items-center flex-1 border-2 border-slate-300 rounded-md p-2">
-          <h2 className="text-2xl text-black">ReactChartJS2</h2>
-          <div>
-            <BarChart categories={categories} households={households} />
-          </div>
-
-          <div>
-            <BarChart categories={categories} households={households} />
-          </div>
+          <h1 className="text-3xl text-black">ReactChartJS</h1>
+          <BarChart categories={categories} households={households} />
+          <BarChart categories={categories} households={households} />
         </div>
 
-        <div className="recharts flex-1 border-2 border-slate-300 rounded-md p-2">
-          <Rechart categories={categories} households={households} />
+        <div className="flex flex-col items-center flex-1 border-2 border-slate-300 rounded-md p-2">
+          <div className="flex flex-col items-center gap-4 pt-2">
+            <h1 className="text-3xl text-black">Recharts</h1>
+            <CustomBarChart categories={categories} households={households} />
+            <CustomPieChart categories={categories} households={households} />
+          </div>
         </div>
       </div>
     </div>
--- src/Rechart/CustomBarChart.tsx
+++ src/Rechart/CustomBarChart.tsx
@@ -2,51 +2,6 @@ import React from "react";
 import { BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Bar } from "recharts";
 
 function CustomBarChart({ categories, households }: PropType) {
-  const [data, setData] = React.useState<AggregateType[]>([]);
-
-  function householdByCategory() {
-    const household_by_category: AggregateType[] = categories.map(
-      (category) => ({ category: category.name, amount: 0 })
-    );
-
-    for (let i = 0; i < 100; i++) {
-      const random_household =
-        households[Math.floor(Math.random() * households.length)];
-
-      // NOTE: Need Refactor - O(100 * categories.length)
-      if (random_household !== undefined) {
-        household_by_category.forEach((h) => {
-          if (h.category == random_household.category.name) {
-            h.amount += random_household.amount;
-          }
-        });
-      }
-    }
-
-    return household_by_category;
-  }
-
-  React.useEffect(() => {
-    document.title = "ReChart";
-    setData(householdByCategory());
-  }, []);
-
-  function get_random_colors() {
-    return [
-      "#C0392B",
-      "#E74C3C",
-      "#9B59B6",
-      "#2980B9",
-      "#1ABC9C",
-      "#16A085",
-      "#2ECC71",
-      "#F1C40F",
-      "#F39C12",
-      "#E67E22",
-      "#D35400",
-    ];
-  }
-
   return (
     <div className="flex flex-col gap-4 p-2 items-center border-2 border-slate-500 rounded-md">
       <h2 className="text-2xl">BarChart</h2>
